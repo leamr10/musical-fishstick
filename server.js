165968425682c0f4866b38f42145394bcafe720c
leamr10 <leamr10@gmail.com>
2023-07-25 10:42:09 -0400

    server added

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'thepassword10',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+db.query('SELECT * FROM employees', function (err, results) {
+  console.log(results);
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
